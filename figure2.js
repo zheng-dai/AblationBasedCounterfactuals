6179661891b012084cce3bd228c108c908bbae4b
zheng-dai <zheng.dai@gmail.com>
2024-06-13 03:01:19 -0400

    Add files via upload

---
+++ figure2.js
@@ -0,0 +1,330 @@
+var figure2 = {}
+figure2.canvas = document.getElementById("figure2")
+figure2.ctx = figure2.canvas.getContext("2d");
+figure2.ctx.imageSmoothingEnabled = false;
+figure2.mousein = false;
+figure2.mousexy = [0, 0];
+figure2.selected = -1;
+figure2.erased = -1;
+figure2.generateindex = 0;
+figure2.forcerender = true;
+figure2.noise = makeNoiseObj(32, 32, 0.1);
+figure2.noisealpha = 0;
+figure2.noisealphamax = 16;
+
+document.getElementById("figure2_button").addEventListener("click", ()=>{
+    figure2.generateindex = (figure2.generateindex + 1)%3;
+    figure2.noisealpha = figure2.noisealphamax;
+    figure2.forcerender = true;
+    figure2.noise.init();
+});
+
+figure2.canvas.addEventListener("mouseenter", (e)=>{
+    figure2.mousein = true;
+    figure2.setcoord(e);
+});
+
+figure2.canvas.addEventListener("mousemove", (e)=>{
+    figure2.setcoord(e);
+});
+
+figure2.canvas.addEventListener("mouseout", ()=>{
+    figure2.selected = -1;
+    figure2.mousein = false;
+});
+
+figure2.canvas.addEventListener("click", ()=>{
+    if (figure2.mousein && figure2.selected != -1){
+        const target = figure2_edges[figure2.selected];
+        let chosen = true;
+        for (let i = 0; i < target.length; i++){
+            const j = target[i];
+            if (!figure2.ablated[j]) {
+                chosen = false;
+                break;
+            }
+        }
+        for (let i = 0; i < figure2.ablated.length; i++){
+            figure2.ablated[i] = false;
+            figure2.abltarget[i] = figure2.ablated_maxsize;
+            figure2.abltarget[16] = 32;
+            figure2.erased = -1;
+        }
+        if (!chosen){
+            figure2.erased = figure2.selected;
+            for (let i = 0; i < target.length; i++){
+                const j = target[i];
+                figure2.ablated[j] = true;
+                figure2.abltarget[j] = 0;
+                figure2.abltarget[16] = 22.6;
+            }
+        }
+    }
+});
+
+figure2.setcoord = (e) => {
+    const rect = figure2.canvas.getBoundingClientRect();
+    const offsetX = e.offsetX/rect.width;
+    const offsetY = e.offsetY/rect.height;
+    figure2.mousexy = [offsetX*figure2.canvas.width, offsetY*figure2.canvas.height];
+}
+
+figure2.drawTraining = (i, xy, s, light) => {
+    const x = xy[0];
+    const y = xy[1];
+
+    const xidx = i%24;
+    const yidx = Math.floor(i/24);
+    figure2.ctx.drawImage(figure2.training, xidx*32, yidx*32, 32, 32, x-s/2, y-s/2, s, s);
+
+    figure2.ctx.globalAlpha = 1-light;
+    figure2.ctx.fillStyle = "black";
+    figure2.ctx.fillRect(x-s/2, y-s/2, s, s);
+    figure2.ctx.globalAlpha = 1;
+}
+
+figure2.drawCounterfactual = (i, j, xy, s) => {
+    const x = xy[0];
+    const y = xy[1];
+    figure2.ctx.drawImage(figure2.generate, i*32, j*32, 32, 32, x-s/2, y-s/2, s, s);
+}
+
+figure2.drawCounterfactual2 = (i, j, xy, s) => {
+    figure2.drawCounterfactual(i, j, xy, s)
+    if (figure2.noisealpha > 0){
+        const a = (1-Math.cos(Math.PI*figure2.noisealpha/figure2.noisealphamax))/2;
+        const x = xy[0];
+        const y = xy[1];
+        figure2.ctx.globalAlpha = a;
+        figure2.ctx.fillStyle = "black";
+        figure2.ctx.fillRect(x-s/2, y-s/2, s, s);
+        figure2.ctx.globalAlpha = a;
+        figure2.ctx.drawImage(figure2.noise.img, 0, 0, 32, 32, x-s/2, y-s/2, s, s);
+        figure2.ctx.globalAlpha = 1;
+    }
+}
+
+figure2.drawFrame = (xy, s) => {
+    const x = xy[0];
+    const y = xy[1];
+    figure2.ctx.strokeStyle = "red";
+    figure2.ctx.strokeRect(x-s/2, y-s/2, s, s);
+}
+
+figure2.check = () => {
+    figure2.selected = -1;
+    if (figure2.mousein){
+        for (let i = 0; i < 384; i++){
+            const x = figure2.training_xyloc[i][0];
+            const y = figure2.training_xyloc[i][1];
+            if (Math.abs(x - figure2.mousexy[0]) <= 20 && Math.abs(y - figure2.mousexy[1]) <= 20){
+                figure2.selected = i;
+            }
+        }
+    }
+
+    let update = figure2.forcerender;
+    figure2.forcerender = false;
+    
+    if (figure2.noisealpha > 0){
+        figure2.noisealpha = figure2.noisealpha-1;
+        figure2.noise.step();
+        update = true;
+    }
+
+    for (let i = 0; i < 384; i++){
+        const z = figure2.renderorder[i][0];
+        const j = figure2.renderorder[i][1];
+
+        if (!figure2.mousein || figure2.selected < 0){
+            if (figure2.natural_z[j] != z){
+                update = true;
+                if (Math.abs(figure2.natural_z[j] - z) < 0.05){
+                    figure2.renderorder[i][0] = figure2.natural_z[j];
+                }
+                else {
+                    figure2.renderorder[i][0] = (figure2.natural_z[j] + z)/2;
+                }
+            }
+        }
+        else {
+            const xy = figure2.training_xyloc[j];
+            const dist = distance_sq(xy, figure2.mousexy);
+            const z_target = Math.exp(-dist/100000);
+            
+            if (figure2.selected == j && z != 1){
+                figure2.renderorder[i][0] = 1;
+                update = true;
+            }
+            else if (figure2.selected != j){
+                if (z_target != z){
+                    update = true;
+                    if (Math.abs(z_target - z) < 0.05){
+                        figure2.renderorder[i][0] = z_target;
+                    }
+                    else {
+                        figure2.renderorder[i][0] = (z_target + z)/2;
+                    }
+                }
+            }
+        }
+    }
+    for (let i = 0; i < figure2.abltrue.length; i++){
+        if (figure2.abltrue[i] != figure2.abltarget[i]){
+            if (Math.abs(figure2.abltrue[i] - figure2.abltarget[i]) < 0.5){
+                figure2.abltrue[i] = figure2.abltarget[i];
+            } else {
+                figure2.abltrue[i] = (figure2.abltrue[i] + figure2.abltarget[i])/2;
+            }
+            update = true;
+        }
+    }
+    return update;
+}
+
+figure2.drawCircle = (xy, r) => {
+    figure2.ctx.beginPath();
+    figure2.ctx.arc(xy[0], xy[1], r, 0, 2 * Math.PI);
+    figure2.ctx.fill();
+}
+
+figure2.loop = () => {
+    if (!figure2.check()){
+        setTimeout(() => requestAnimationFrame(figure2.loop), 200);
+        return;
+    }
+    figure2.renderorder.sort(function(a, b) { return a[0] - b[0]; });
+    figure2.ctx.clearRect(0, 0, figure2.canvas.width, figure2.canvas.height);
+
+    figure2.ctx.lineWidth = 1;
+    for (let pre_i = 0; pre_i < 384; pre_i++){
+        const i = figure2.renderorder[pre_i][1];
+        const z = figure2.selected == i ? 1 : figure2.renderorder[pre_i][0];
+
+        const size = z*16 + 24;
+        const brightness = (i == figure2.erased) ? 0 : z/2 + 0.5;
+
+        let segments = 20;
+        if (figure2.selected != i){
+            figure2.ctx.lineWidth =  1;
+            figure2.ctx.strokeStyle = "black";
+            figure2.ctx.globalAlpha = z*0.3 + 0.2;
+        }
+        else {
+            figure2.ctx.strokeStyle = "red";
+            figure2.ctx.globalAlpha = 1;
+            figure2.ctx.lineWidth =  8;
+            figure2.drawFrame(figure2.training_xyloc[i], size);
+            figure2.ctx.lineWidth =  3;
+            segments = 100;
+        }
+        const targets = figure2_edges[i];
+        for (let j = 0; j < targets.length; j++){
+            const k = targets[j];
+            if (!figure2.ablated[k]) figure2.lineto(figure2.training_xyloc[i], figure2.param_xyloc[k], segments);
+        }
+        figure2.ctx.globalAlpha = 1;
+        figure2.drawTraining(i, figure2.training_xyloc[i], size, brightness);
+    }
+    let flow = false;
+    for (let i = 0; i < 16; i++){
+        figure2.ctx.lineWidth = 2;
+        figure2.ctx.strokeStyle = "black";
+        figure2.ctx.fillStyle = "black";
+        if (figure2.selected != -1){
+            for (let j = 0; j < figure2_edges[figure2.selected].length; j++){
+                if (i == figure2_edges[figure2.selected][j]){
+                    figure2.ctx.lineWidth = 5;  
+                    figure2.ctx.strokeStyle = "red";
+                    figure2.ctx.fillStyle = "red";
+                    if (!figure2.ablated[i]) flow = true;
+                    break;
+                }
+            }
+        }
+        if (!figure2.ablated[i]) figure2.lineto2(figure2.param_xyloc[i], figure2.combined_xy);
+        figure2.drawCircle(figure2.param_xyloc[i], figure2.abltrue[i])
+    }
+    if (!flow){
+        figure2.ctx.strokeStyle = "black";
+        figure2.ctx.fillStyle = "black";
+        figure2.ctx.lineWidth = 5;
+    } else {
+        figure2.ctx.strokeStyle = "red";
+        figure2.ctx.fillStyle = "red";
+        figure2.ctx.lineWidth = 9;
+        figure2.drawFrame(figure2.generate_xy, 64);
+    }
+
+    figure2.lineto2(figure2.combined_xy, figure2.generate_xy);
+    figure2.drawCircle(figure2.combined_xy, figure2.abltrue[16]);
+    figure2.drawCounterfactual2(figure2.erased + 1, figure2.generateindex, figure2.generate_xy, 64);
+
+    setTimeout(() => requestAnimationFrame(figure2.loop, false), 80);
+}
+
+figure2.lineto = (xy1, xy2, segments) => {
+    drawline(xy1[0], xy1[1], xy2[0], xy2[1], segments, interpolate_cosine, figure2.ctx);
+}
+
+figure2.lineto2 = (xy1, xy2) => {
+    drawline(xy1[0], xy1[1], xy2[0], xy2[1], 100, interpolate_square, figure2.ctx);
+}
+
+figure2.lineto3 = (xy1, xy2) => {
+    drawline(xy1[0], xy1[1], xy2[0], xy2[1], 1, (x) => x, figure2.ctx);
+}
+
+figure2.init = () => {
+    loadImages(["website/web1.png", "website/web2.png"], (images) => {
+        figure2.training = images[0];
+        figure2.generate = images[1];
+
+        const xcenter = 340;
+        const ycenter = 500;
+        figure2.training_xyloc = [];
+        figure2.sizes = [];
+        figure2.natural_z = [];
+        figure2.renderorder = [];
+        for (let i = 0; i < 384; i++){
+            const x_raw = i%16;
+            const y_raw = Math.floor(i/16);
+            const x = xcenter + (x_raw-7.5)*40;
+            const y = ycenter + (y_raw-11.5)*40;
+            figure2.training_xyloc.push([x, y]);
+            figure2.sizes.push(32);
+
+            const ydelta = (1-Math.pow((y_raw-11.5)/11.5, 2))*0.2;
+            const xdelta = (x_raw/15)*0.8;
+            const z = ydelta + xdelta;
+            figure2.natural_z.push(z);
+            figure2.renderorder.push([z, i]);
+        }
+
+        const xcenter2 = 1000;
+        const ycenter2 = ycenter;
+        figure2.param_xyloc = [];
+        figure2.ablated = [];
+        figure2.abltarget = [];
+        figure2.abltrue = [];
+        figure2.ablated_maxsize = 8;
+        for (let i = 0; i < 16; i++){
+            const x = xcenter2;
+            const y = ycenter2 + (i-7.5)*50;
+            figure2.param_xyloc.push([x, y]);
+            figure2.ablated.push(false);
+            figure2.abltarget.push(figure2.ablated_maxsize);
+            figure2.abltrue.push(figure2.ablated_maxsize);
+        }
+        figure2.abltarget.push(32);
+        figure2.abltrue.push(32);
+
+        figure2.combined_xy = [1240, ycenter];
+        figure2.generate_xy = [1340, ycenter];
+
+        figure2.forcerender = true;
+        figure2.loop();
+    })
+}
+
+figure2.init();
---
+++ utils.js
@@ -0,0 +1,106 @@
+
+function loadImages(filenames, callback)
+{
+    function loadImage(filenames, images, callback)
+    {
+        if (filenames.length == 0) callback(images);
+        else
+        {
+            const filename = filenames.pop();
+            const img = new Image();
+            images.push(img);
+            img.onload = function() {
+                loadImage(filenames, images, callback)
+            };
+            img.src = filename;
+        }
+    }
+
+    filenames.reverse();
+    loadImage(filenames, [], callback);
+}
+
+function interpolate_cosine(p)
+{
+    const p2 = (1 - Math.cos(p*Math.PI))/2;
+    return p2;
+}
+
+function interpolate_square(p)
+{
+    const p2 = 1-Math.pow(1-p, 2);
+    return p2;
+}
+
+function drawline_helper(x1, y1, x2, y2, ctx)
+{
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2)
+    ctx.stroke();
+}
+
+function drawline(x1, y1, x2, y2, n, interpolate, ctx)
+{
+    const xs = [];
+    const ys = [];
+    for (let i = 0; i <= n; i++)
+    {
+        const p = i/n;
+        const q = 1-p;
+        const p2 = interpolate(p, 2);
+        const q2 = 1 - p2;
+        xs.push(x1*p + x2*q);
+        ys.push(y1*p2 + y2*q2);
+    }
+    for (let i = 0; i < n; i++)
+    {
+        drawline_helper(xs[i], ys[i], xs[i+1], ys[i+1], ctx);
+    }
+}
+
+function distance_sq(xy1, xy2){
+    dx = Math.pow(xy1[0] - xy2[0], 2);
+    dy = Math.pow(xy1[1] - xy2[1], 2);
+    return dx + dy
+}
+
+function makeNoiseObj(width, height, sigma){
+    const noiseObj = {};
+    
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas.getContext('2d');
+
+    const idata = ctx.createImageData(width, height);
+    const buffer = new Uint32Array(idata.data.buffer);
+    const floatbuffer = [];
+    for (let i = 0; i < buffer.length; i++){
+        floatbuffer.push(0);
+    }
+
+    noiseObj.img = canvas;
+    noiseObj.step = () => {
+        let len = buffer.length - 1;
+        while(len--){
+            //draw normal rv
+            const u = 1 - Math.random();
+            const v = Math.random();
+            const z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v )
+            //brownian motion
+            floatbuffer[len] = Math.max(Math.min( ((bufferFloat[len] + (z*sigma) - 0.5)*0.95) + 0.5, 0.8 ), 0.2);
+            const intBuffer = ( (Math.floor(floatbuffer[len] * 256)) % 256 ) >>> 0;
+            buffer[len] = (intBuffer<<0) | (intBuffer<<16) | (intBuffer<<8) | ((255>>>0)<<24);
+        }
+        ctx.putImageData(idata, 0, 0);
+    }
+    noiseObj.init = () => {
+        for (let i = 0; i < buffer.length; i++){
+            floatbuffer[i] = (Math.random()*0.1) + 0.45;
+        }
+        noiseObj.step();
+    }
+    noiseObj.init();
+    return noiseObj;
+}
